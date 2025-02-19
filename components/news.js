7ab95169b268a8144e5d5790b9424f586f00ee2e
Hajime Dango <hajimedangovt@gmail.com>
2025-02-19 13:53:14 -0500

    Update news.js

--- components/news.js
+++ components/news.js
@@ -11,9 +11,10 @@ class News extends HTMLElement {
               <h2>Debut announcement soon! But first... </h2>
               <p>"One Trick Pony" out now on all major streaming platforms!
               </p>
-              <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/6rBy8i2Tjiwjo3W51tk8hN?utm_source=generator&theme=0" width="100%" height="200" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>  
-              <iframe width="100%" height="315" src="https://www.youtube.com/embed/33UaiPcuNi8?si=Vd-NRXOk9554dDVz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
+              <iframe width="50%" height="300" src="https://www.youtube.com/embed/33UaiPcuNi8?si=Vd-NRXOk9554dDVz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <br> 
+              <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/6rBy8i2Tjiwjo3W51tk8hN?utm_source=generator&theme=0" width="50%" height="200" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> 
+            
             </div>
           </div>
         `;
