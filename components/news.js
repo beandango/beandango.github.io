01964557268ac13105b286b8d3900f07c93b3e01
Hajime Dango <hajimedangovt@gmail.com>
2025-02-28 20:55:52 -0500

    Update news.js

--- components/news.js
+++ components/news.js
@@ -8,16 +8,7 @@ class News extends HTMLElement {
           <div class="box">
           <h1>Latest News!</h1>
             <div class="inner">
-              <h2>Debut On March 8, 2025!!!</h2>
-              <p>
-                Cherry and I will be debuting our new group BIZARRE RENAISSANCE on March 8th! <br><br>
-                <b>Featuring: </b> <br><br>
-                1 new original song <br>
-                2 new covers <br>
-                3 new interactive websites with fun secrets and lore <b>(You can reach all of them by clicking the cute stamps at the top of the sidebar!)</b>  <br><br>
-                More info to come in the coming days!
-              </p>
-            
+              <iframe width="100%" height="350px" src="https://e.notionhero.io/e1/p/b0f3d8c-7f3ef3017c0d2214747dba5d4feb560"></iframe>
             </div>
           </div>
         `;
